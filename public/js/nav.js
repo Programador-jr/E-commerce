206f1ea34b896335f38450b06d7b6eddf0608153
King <70107167+Programador-jr@users.noreply.github.com>
2021-12-15 05:25:07 +0000

    Server creation with nodemon

--- public/js/nav.js
+++ public/js/nav.js
@@ -3,14 +3,14 @@ const createNav = () => {
 
     nav.innerHTML = `
         <div class="nav">
-            <a href=""><img src="public/img/logo.png" class="brand-logo" alt=""></a>
+            <a href=""><img src="img/logo.png" class="brand-logo" alt=""></a>
             <div class="nav-items">
                 <div class="search">
                     <input type="text" class="search-box" placeholder="Pesquisar Produto">
                     <button class="search-btn">Procurar</button>
                 </div>
-                <a href="#"><img src="public/img/user.png" alt=""></a>
-                <a href="#"><img src="public/img/cart.png" alt=""></a>
+                <a href="#"><img src="img/user.png" alt=""></a>
+                <a href="#"><img src="img/cart.png" alt=""></a>
             </div>
         </div>
         <ul class="links-container">
